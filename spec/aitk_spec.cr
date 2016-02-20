9b27c84664903aef0e7f8c660d52be09b694e0e2
Potapov Sergey <blake131313@gmail.com>
2016-02-20 19:01:43 +0100

    Implement Euclidean, Manhattan and Chebyshev distances

--- spec/aitk_spec.cr
+++ spec/aitk_spec.cr
@@ -2,13 +2,38 @@ require "./spec_helper"
 
 describe Aitk do
   describe "#euclidean_distance" do
-    context "vector size mismatch" do
+    context "vector length mismatch" do
+      it "raises ArgumentError" do
+        expect_raises(ArgumentError, "Vector length mismatch") do
+          Aitk.euclidean_distance([0], [1, 2])
+        end
+      end
     end
 
     context "vector size matches" do
       it "calculates distance" do
+        Aitk.euclidean_distance([] of Float64, [] of Float64).should eq 0
         Aitk.euclidean_distance([0], [1]).should eq 1
+        Aitk.euclidean_distance([-1, 2], [2, 6]).should eq 5
       end
+    end
+  end
+
+  describe "#manhattan_distance" do
+    it "calculates" do
+      Aitk.manhattan_distance([] of Float64, [] of Float64).should eq 0
+      Aitk.manhattan_distance([0], [5]).should eq 5
+      Aitk.manhattan_distance([0, 2], [5, 4]).should eq 7
+      Aitk.manhattan_distance([0, 2, 3], [5, 4, -1]).should eq 11
+    end
+  end
+
+  describe "#chebyshev_distance" do
+    it "returns the max distance in same dimension" do
+      Aitk.chebyshev_distance([] of Float64, [] of Float64).should eq 0
+      Aitk.chebyshev_distance([0], [5]).should eq 5
+      Aitk.chebyshev_distance([0, 2], [5, 4]).should eq 5
+      Aitk.chebyshev_distance([0, 2, -3], [5, 4, 6.5]).should eq 9.5
     end
   end
 end
--- src/aitk.cr
+++ src/aitk.cr
@@ -1,7 +1,38 @@
 require "./aitk/*"
 
-module Aitk
-  def euclidean_distance(vec1 : Array(Numeric), vec2 : Array(Numeric)) : Numeric
+require "math"
 
+module Aitk
+  extend self
+
+  def euclidean_distance(vec1 : Array, vec2 : Array)
+    raise ArgumentError.new("Vector length mismatch") if vec1.size != vec2.size
+
+    sum = 0.0
+    vec1.size.times do |index|
+      sum += (vec1[index] - vec2[index]) ** 2
+    end
+    Math.sqrt(sum)
+  end
+
+  def manhattan_distance(vec1 : Array, vec2 : Array)
+    raise ArgumentError.new("Vector length mismatch") if vec1.size != vec2.size
+
+    distance = 0.0
+    vec1.size.times do |index|
+      distance += (vec1[index] - vec2[index]).abs
+    end
+    distance
+  end
+
+  def chebyshev_distance(vec1 : Array, vec2 : Array)
+    raise ArgumentError.new("Vector length mismatch") if vec1.size != vec2.size
+
+    max_distance = 0.0
+    vec1.size.times do |index|
+      current_distance = (vec1[index] - vec2[index]).abs
+      max_distance = current_distance if current_distance > max_distance
+    end
+    max_distance
   end
 end
