7c5d39f054c3e12fca2117f4f27c6ed9fda0affc
Potapov Sergey <blake131313@gmail.com>
2016-02-27 20:26:58 +0100

    Use Euclidean distance for trainers

--- src/aitk/rbf/trainers/base_trainer.cr
+++ src/aitk/rbf/trainers/base_trainer.cr
@@ -8,13 +8,7 @@ module Aitk
         @training_set.each do |data|
           input, ideal_output = data
           output = @network.call(input)
-
-
-          a, b = 1.0, 1.0
-          ideal_output = [ideal_output[0], ideal_output[1]*a, ideal_output[2]*b]
-          output = [output[0], output[1]*a, output[2]*b]
-
-          score += Aitk.manhattan_distance(output, ideal_output)
+          score += Aitk.euclidean_distance(output, ideal_output)
         end
         score
       end
